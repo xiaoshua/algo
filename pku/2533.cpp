27afa875afca5a99a680e49ff9e578f430ab5194
momei <momei@wacai.com>
2016-04-17 00:30:34 +0800

    code format

--- pku/2533.cpp
+++ pku/2533.cpp
@@ -1,29 +1,32 @@
 #include<cstdio>
 #include<algorithm>
+
 using namespace std;
+
 const int maxn = 1 << 10;
 int a[maxn], dp[maxn];
+
 int main() {
-	int i, j, n, ans;
+    int i, j, n, ans;
 
-	while(scanf("%d", &n) != EOF) {
-		for(i = 0; i < n; ++i) {
-			scanf("%d", a + i);
-		}
+    while (scanf("%d", &n) != EOF) {
+        for (i = 0; i < n; ++i) {
+            scanf("%d", a + i);
+        }
 
-		fill(dp, dp + n, 1);
+        fill(dp, dp + n, 1);
 
-		for(i = 0; i < n; ++i) {
-			for(j = 0; j < i; ++j) {
-				if(a[j] < a[i]) {
-					dp[i] = max(dp[i], dp[j] + 1);
-				}
-			}
-		}
+        for (i = 0; i < n; ++i) {
+            for (j = 0; j < i; ++j) {
+                if (a[j] < a[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
 
-		ans = *max_element(dp, dp + n);
-		printf("%d\n", ans);
-	}
+        ans = *max_element(dp, dp + n);
+        printf("%d\n", ans);
+    }
 
-	return 0;
+    return 0;
 }
